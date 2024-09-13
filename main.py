505683060d2575ee6a128a9c4c16ba618f3619b6
Hayden <haydenfarrell@outlook.com>
2024-09-13 19:39:30 +0100

    form submits

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def querry():
+    # connect to DB
+
+    # Querry value
+
+    # format result
+
+    # return to user
+
+    return render_template('index.html')
